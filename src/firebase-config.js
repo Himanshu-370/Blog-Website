6ccc864b86f9f41cafc60ec572e32bc2d375eb69
Himanshu Singh <himanshuich20@gmail.com>
2022-06-21 18:23:20 +0530

    inital commit

---
+++ src/firebase-config.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB0tDVaUGs2RpADGUgXW8rhvaWKJjJ58C8",
+  authDomain: "blog-website-project-a8071.firebaseapp.com",
+  projectId: "blog-website-project-a8071",
+  storageBucket: "blog-website-project-a8071.appspot.com",
+  messagingSenderId: "457803636070",
+  appId: "1:457803636070:web:9099c56d9c5fb35d7efca7",
+  measurementId: "G-4YMVWTPNGG",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
---
+++ src/pages/Home.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from "react";
+import { getDocs, collection, deleteDoc, doc } from "firebase/firestore";
+import { auth, db } from "../firebase-config";
+import { reload } from "firebase/auth";
+
+const Home = ({ isAuth }) => {
+  const [postLists, setPostLists] = useState([]);
+
+  const postsCollectionRef = collection(db, "posts");
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(postsCollectionRef);
+      setPostLists(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getPosts();
+  }, []);
+
+  const deletePost = async (id) => {
+    const postDoc = doc(db, "posts", id);
+    await deleteDoc(postDoc);
+    window.location.reload(false);
+  };
+
+  const current = new Date();
+  const date = `${current.getDate()}/${
+    current.getMonth() + 1
+  }/${current.getFullYear()}`;
+
+  return (
+    <div className="homePage">
+      {postLists.map((post) => {
+        return (
+          <div className="post">
+            <div className="postHeader">
+              <div className="title">
+                <h1>{post.title}</h1>
+              </div>
+              <div className="deletePost">
+                {isAuth && post.author.id === auth.currentUser.uid && (
+                  <button
+                    onClick={() => {
+                      deletePost(post.id);
+                    }}
+                  >
+                    &#128465;
+                  </button>
+                )}
+              </div>
+            </div>
+            <div className="postTextContainer">{post.postText}</div>
+            <h5>{date}</h5>
+            <h5>@ {post.author.name}</h5>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Home;
